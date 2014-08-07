9f7c459d9429332f592f2ff77586edebba8c8b6d
Anthony saldana <anthonysaldana03@gmail.com>
2014-08-07 00:12:10 -0700

    Main code for login form
    This is the main code for the login form on the coding sailor blog. Code contains includes for functions and other files that are used here.

--- loginform.php
+++ loginform.php
@@ -1,16 +1,18 @@
 <?php
 require_once'secure/logindb.php';
-include_once("secure/mysqlfatalerror.php");
-$db_server = mysql_connect($db_hostname, $db_username, $db_password);
+include_once("secure/mysqlfatalerror.php"); 
+// function inside here incase of error. Creates a client friendly error message
+$db_server = mysql_connect($db_hostname, $db_username, $db_password); //connect to database
 
-if(!$db_server) 
+if(!$db_server) //if connection is unsuccessful this will display a client friendly error message. 
 	{
 		$errormsg="Database failed to connect"; 
 		die(mysql_fatal_error($errormsg));
 	}
 	
 echo"Coding Sailor is currently under construction.";
-$htmllogin = <<<_codinghtml
+$htmllogin = <<<_codinghtml //HTML code for the login form
+
 <html>
 	<head><link rel="stylesheet" type="text/css" href="../styles/login.css"> 
 </head>
@@ -26,5 +28,5 @@ $htmllogin = <<<_codinghtml
 </html>
 _codinghtml;
 
-echo $htmllogin;
+echo $htmllogin; //displays the login form
 ?>
