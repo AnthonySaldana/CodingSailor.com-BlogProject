453a82a1c1cdff1048a0c3bb18cbebd838eb0952
Anthony saldana <anthonysaldana03@gmail.com>
2014-09-05 16:14:30 -0700

    Profile info
    Displays the information for the user logged in. Will be adding a feature that allows to edit this information since most of this info isn't asked for on sign up.

--- user/profileview.php
+++ user/profileview.php
@@ -1,6 +1,39 @@
 <?php
 require_once ($root . '/user/userclass.php');
 $user =  new user;
-$test = $user->username;
-print_r($user);
+echo<<<profilehtml
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<h2>$user->username</h2>
+				<img class="img-circle" alt="Circular Image"/>
+				<p class="h4">$user->realname</p>
+				<p class="h4">Email: $user->useremail</p>
+				<p class="h4">Date Joined: $user->datejoined</p>
+				<p class="h4">Website: $user->userwebsite</p>
+			</div>
+			
+			<div class="col-md-6" style="text-align:right;">
+				<h2>About me</h2>
+				<p class="h4">Country: $user->usercountry</p>
+				<p class="h4">State: $user->userstate</p>
+				<p class="h4">City: $user->usercity</p>
+				<p class="h4">DOB: $user->dateofbirth</p>
+					<div class="panel panel-default">
+						<div class="panel-body"><p class="h4">$user->userabout</p></div>
+					</div>
+			</div>
+		</div>
+		
+		<div class="row">
+			<center><div class="col-md-12">
+				<form action="editprofile.php" method="POST">
+					<button class="btn btn-info" type="submit">Edit Profile</button>
+				</form>
+			</div></center>
+		</div>
+	</div>
+</body>
+profilehtml;
 ?>
