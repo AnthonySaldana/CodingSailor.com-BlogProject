de67b2f6d2fcf9eccadb3ac5f2545e966706b669
Anthony saldana <anthonysaldana03@gmail.com>
2014-09-08 11:03:17 -0700

    Create index.php

---
+++ posts/index.php
@@ -0,0 +1,7 @@
+<?php
+$tag = "posts";
+$root = $_SERVER['DOCUMENT_ROOT'];
+include_once($root . "/header.php");
+include_once($root . "/posts/current.php");
+include_once($root. "/footer.php");
+?>
