335ddd48976f9d0d6c83126cfb982f4dff5028b7
Anthony saldana <anthonysaldana03@gmail.com>
2014-09-09 14:57:38 -0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,10 +2,49 @@
 $root = $_SERVER['DOCUMENT_ROOT'];
 ?>
 <head>
-<link href="styles/footer.css" rel="stylesheet" type="text/css" >
-</head><body>
-<div class="navbar navbar-default navbar-fixed-bottom" id="footer">
-<div class="container" ><center><div class="row">	<div class="col-md-4">
-	<a href="index.php">Home</a></br>	<a href="index.php">about</a></br>	<a href="index.php">Home</a></br>	</div></div>
-</center></div>
-</div></body>
+<link href="/styles/footer.css" rel="stylesheet" type="text/css" >
+</head>
+<body>
+<!-- fb like js-->
+<div id="fb-root"></div>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=437392143066826&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<!-- end of fb js-->
+<div class="navbar-default" id="footer">
+	<div class="container" >
+		<center>
+		<div class="row">	
+			<div class="col-md-4 footerlinks">
+				<a href= "/index.php">Home</a></br>
+				<a href="/about.php">About</a></br>
+				<a href="/posts/">Posts</a>
+			</div>
+			<div class="col-md-4 defenseleague">
+				<a href="https://internetdefenseleague.org"><img src="https://internetdefenseleague.org/images/badges/final/shield_badge.png" alt="Member of The Internet Defense League"/></a>
+			</div>
+			<div class="col-md-4 footerlinks">
+				<!-- fb like-->
+				<div class="fb-like" data-href="https://www.facebook.com/pages/Coding-Sailor/204306013035081?ref=hl" data-layout="box_count" data-action="like" data-show-faces="true" data-share="true"></div>
+				<!-- end of fb like -->
+				
+				<!-- twitter follow button -->
+				<a href="https://twitter.com/anthonysaldana3" class="twitter-follow-button" data-show-count="false">Follow @anthonysaldana3</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+			</div>
+		</div>
+		
+	</div>
+	<div class="row codingfooter">
+		<center>
+			<h3>Copyright <a href="https://twitter.com/anthonysaldana3" style="color:rgba(170,170,170,1);">Anthony Saldana</a> &copy; 2014</h3>
+		</center>
+		</div>
+		</center>
+</div>
+</body>
