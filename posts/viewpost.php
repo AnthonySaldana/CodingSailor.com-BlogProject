a3e47309451163bad723cb547c8d3bfa2b3cc63a
Anthony saldana <anthonysaldana03@gmail.com>
2014-09-09 14:56:53 -0700

    Update viewpost.php

--- posts/viewpost.php
+++ posts/viewpost.php
@@ -1,4 +1,5 @@
 <?php
+$tag = "posts";
 $root = $_SERVER['DOCUMENT_ROOT'];
 include_once $root . '/header.php';
 require_once($root . '/loginform/secure/logindb.php');
@@ -16,16 +17,19 @@ $resultcurrent=mysql_query($latestquery);
 if(!$resultcurrent) die("couldnt read posts");
 $row = mysql_fetch_row($resultcurrent);
 echo<<<_currentposts
-		<div class='container'>
+		<div class='container' style="margin-top:20px; margin-bottom:20px;">
 			<div class='row'>
-				<div class='col-md-12'>
+				<div class='col-md-8'>
 					<div class='list-group'>
-						<a href='/posts/viewpost.php?postid=$row[3]' class='list-group-item'>
-						<h2 Class='list-group-item-heading'>$row[1]</h2>
-						<p>$row[2]</p>
+						<h1>$row[1]</h1>
+						<p class="h4" style="line-height:150%;">$row[2]</p>
 						<p>$row[4]</p></a>
+						<br/>
+						<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://codingsailor.com/posts/viewpost.php?postid=$postid" data-via="anthonysaldana3">Tweet</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 					</div>
 				</div>
+				
 			</div>
 		</div>
 _currentposts;
