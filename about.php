bb24b1d48c54722a0c67b9dd7fa5a7c0c30a30b9
Anthony saldana <anthonysaldana03@gmail.com>
2014-09-09 14:56:09 -0700

    Create about.php

---
+++ about.php
@@ -0,0 +1,78 @@
+<?php
+$tag="about";
+$root = $_SERVER['DOCUMENT_ROOT'];
+include_once($root . "/header.php");
+
+?>
+<head>
+	<link href="/styles/about.css" rel="stylesheet" type="text/css" />
+	<title>Coding Sailor - About</title>
+	<meta name='description' content='About the coding sailor'>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+
+<body>
+<div class="container aboutcontainer">
+	<div class="row">
+		<div class="col-md-8">
+				<h1>About Me & Coding Sailor</h1>
+				<p class="h4 abouttext">My name is Anthony Saldana. I have come to be known by the nickname Arbie. 
+				I currently (September of 2014) live in Whittier, with my fiancée and son.
+				I'm a web developer. I've always loved technology and more specifically, computers.
+				Since a child I was fascinated by them. I would have to say that the path to me becoming
+				a programmer began when I took apart my computer for the first time. In some way I became 
+				extremely fascinated with what was going on inside electronics (not just computers). 
+				<br/><br/>
+				In middle school I played the guitar (not too good at it) and I had a friend who taught me 
+				how to take apart an amp and change parts to make it distorted. ( I liked rock and metal music.. still do.)
+				I would go into my amp and rewire everything with different speakers and I was having fun.
+				This really shot me in the right direction. Like I said before, I was amazed at how things worked from
+				the inside. This led me to taking apart an old laptop just to see how things looked inside it.
+				With one dead laptop and a dinosaur desktop the seed had been planted. 
+				<br/><br/>
+				Now jump forward a few years, I grabbed my first programming book during the end of high school. I think it was a few months before my
+				senior year. The book was 
+				<a href="http://www.amazon.com/Beginning-Programming-C-For-Dummies/dp/0470617977/ref=sr_1_1?ie=UTF8&qid=1410280616&sr=8-1&keywords=programming+with+c%2B%2B+for+dummies" target="_blank">
+				Beginning Programming with c++ for Dummies by Stephen R. Davis.</a> It was a good book and I learned 
+				things that helped me along the next few years. Now I dont use c++ today, but the concepts remain.
+				During my Senior year in high school I took a c++ programming class at my local community college ( Rio Hondo College). ACtually I took two classes during that year.
+				An intro and advanced c++ class. Anyway thats what began this path. During that time I knew that I wanted to write code.
+				<br/><br/>
+				I'm not really studying the C languages anymore, I had a little run with c# but now I'm running with web development. My passion lies at the back end of 
+				web development, PHP and MySQL are what I really want to master right now. I have decent skills in the front end, but I want to program web applications, not so much design.
+				</p>
+				
+				<h1>Why Blog?</h1>
+				<p class="h4 abouttext">
+					I blog because I like to write. I use to have a blog where I wrote advice but eventually I just stop keeping up
+					with it and let it die. I started this blog for a few reasons:</br></p>
+					<ol class="h5 abouttext">
+						<li>Develop a bloggin platform for experience - I developed this blog from scratch using php. Everything is hand crafted by yours truly.</li>
+						<li>Write!! - Like I Said, I like writing. In fact I love it. I wanna write books someday. I also love reading, that might explain why i love writing..</li>
+						<li>Learn. - What better way to better my development skills than to write about it!</li>
+						<li>Help - This isn't just a tech blog. Alot of what I post will be advice, life lessons I've learned during the short years on this planet.</li>
+					</ol>
+				
+				<h1>Where's this blog headed?</h1>
+				<p class="h4 abouttext">
+					My plans for this blog isn't just to write. I also have plans to make podcasts and video podcasts in the near future, So 
+					I am very excited for that! For podcasts I hope to do things like talk about specific tech topics, give career advice, interview cool programmers and 
+					just cool people in general, and maybe more if it comes to mind. I'm just giving an overview.
+				</p>
+				
+				<br/>
+				<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://codingsailor.com/about.php" data-via="anthonysaldana3" data-size="large">Tweet</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+
+				<div class="fb-like" data-href="http://codingsailor.com/about.php" data-layout="standard" data-action="recommend" data-show-faces="true" data-share="true"></div>
+		</div>
+		<div class="col-md-4">
+			<img src="/images/users/aboutme.jpg" width="100%"/>
+			<h2><u>Programmer</u><br/>pro·gram·mer- noun: An organism capable of converting caffeine into code.</h2>
+		</div>
+	</div>
+</div>
+</body>
+<?php
+include_once($root . "/footer.php");
+?>
