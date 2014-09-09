91e3c3c694a3d03762b210600c1723bf573ef721
Anthony saldana <anthonysaldana03@gmail.com>
2014-09-09 14:55:30 -0700

    Update index.php

--- index.php
+++ index.php
@@ -6,18 +6,15 @@ echo<<<_headerhtml
 <head>
 <link rel="stylesheet" href="styles/index.css" type="text/css" />
 <title>Coding Sailor</title>
-<meta name='description' content='The coding sailor blog! A world of tech, help, and
-my life - arbie'>
+<meta name='description' content='The coding sailor blog! A world of tech, help, and common sense'>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 </head>
-<div class="jumbotron" style="background-color:rgba(50,75,100,1); height:400px;">
-<div class="imgcontainer">
+<div class="jumbotron imgcontainer" style="background-color:rgba(50,75,100,1); height:400px;">
 	<div class="container" style="color:white; margin-top:70px;">
 		<center><h1>Hello, i'm arbie</h1>
 		<p>Welcome to the coding sailor! A blog that deals with tech and a dash
-		of common sense.</p>
+		of common sense.</p></center>
 	</div>
-</div>
 </div>
 _headerhtml;
 
