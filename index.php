785ab294378d7326644e5242b4d475d793d6d004
Anthony saldana <anthonysaldana03@gmail.com>
2014-11-21 09:41:47 -0800

    UI Updates
    Made a few tweaks to the style of the home page. Also limited the home page to show the last 6 blog posts.

--- index.php
+++ index.php
@@ -9,15 +9,28 @@ echo<<<_headerhtml
 <meta name='description' content='The coding sailor blog! A world of tech, help, and common sense'>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 </head>
-<div class="jumbotron imgcontainer" style="background-color:rgba(50,75,100,1); height:400px;">
+<div class="jumbotron imgcontainer" style="background-color:rgba(50,75,100,1);">
 	<div class="container" style="color:white; margin-top:70px;">
-		<center><h1>Hello, i'm arbie</h1>
+		<div class="row">
+		<div class="col-md-12">
+		<center>
+		
+		<h1>Hello, i'm arbie</h1>
 		<p>Welcome to the coding sailor! A blog that deals with tech and a dash
-		of common sense.</p></center>
+		of common sense.</p>
+		</center>
+		</div>
+		</div>
 	</div>
 </div>
+
 _headerhtml;
 
 include_once($root . "/posts/current.php");
+?>
+<hr/>
+<center><a href="posts/" class='h2'> View All Posts... </a></center>
+<hr/>
+<?php
 include_once($root . "/footer.php");
 ?>
