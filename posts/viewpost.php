06c024ecf56fbce5ac62bbc793ce6443f2eb6f71
Anthony saldana <anthonysaldana03@gmail.com>
2014-11-21 09:49:12 -0800

    Title update
    Added title for each blog post based on blog title

--- posts/viewpost.php
+++ posts/viewpost.php
@@ -17,21 +17,24 @@ $resultcurrent=mysql_query($latestquery);
 if(!$resultcurrent) die("couldnt read posts");
 $row = mysql_fetch_row($resultcurrent);
 echo<<<_currentposts
-		<div class='container' style="margin-top:20px; margin-bottom:20px;">
-			<div class='row'>
-				<div class='col-md-8'>
-					<div class='list-group'>
-						<h1>$row[1]</h1>
-						<p class="h4" style="line-height:150%;">$row[2]</p>
-						<p>$row[4]</p></a>
-						<br/>
-						<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://codingsailor.com/posts/viewpost.php?postid=$postid" data-via="anthonysaldana3">Tweet</a>
+<head>
+<title>$row[1]</title>    
+</head>
+<div class='container' style="margin-top:20px; margin-bottom:20px;">
+    <div class='row'>
+        <div class='col-md-8'>
+            <div class='list-group'>
+                    <h1>$row[1]</h1>
+
+                    <p class="h4" style="line-height:150%;">$row[2]</p>
+                    <p>$row[4]</p></a>
+                    <br/>
+                    <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://codingsailor.com/posts/viewpost.php?postid=$postid" data-via="anthonysaldana3">Tweet</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-					</div>
-				</div>
-				
-			</div>
-		</div>
+            </div>
+        </div>
+    </div>
+</div>
 _currentposts;
 include_once $root . '/footer.php';
 ?>
