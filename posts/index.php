e7cdeb950eb2042ed54e045e0af4c30f321b5f76
Anthony saldana <anthonysaldana03@gmail.com>
2014-11-21 09:44:23 -0800

    Title update
    Added page title to the posts page.

--- posts/index.php
+++ posts/index.php
@@ -1,8 +1,11 @@
+<head>
+    <title>Coding Sailor Blog Posts</title>
+</head>
 <?php
 $tag = "posts";
 $root = $_SERVER['DOCUMENT_ROOT'];
 include_once($root . "/header.php");
-echo"<br/>";
+echo"<br/><center><h1 class='h2'>All Coding Sailor Blog Posts</h1></center> <hr/>";
 include_once($root . "/posts/current.php");
 include_once($root. "/footer.php");
 ?>
